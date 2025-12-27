18803ecbfeadaf0df4fe5c0f0429236f34ae71e6
savitacb7557 <savitacb7557@gmail.com>
2025-12-27 17:16:18 +0000

    commit after testing

--- src/main/java/com/tcs/payments/PaymentsApplication.java
+++ src/main/java/com/tcs/payments/PaymentsApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import com.tcs.payments.service.PaymentsProperties;
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
+
 @EnableConfigurationProperties(PaymentsProperties.class)
 public class PaymentsApplication {
 
